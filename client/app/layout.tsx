c6eb9be5a3b41f81db50c18a058fd4ed8839dbd0
Theo <theorusu11@gmail.com>
2024-09-19 16:38:49 -0400

    init: setup nextjs and chakra ui

---
+++ client/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Providers } from "./provider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SwiftBuy",
+  description: "eCommerce website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
